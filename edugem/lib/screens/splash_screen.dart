d6100c8e631e27639a132d3ca3e90576e1bbd65f
Byron94odhiambo <odhiambobyron39@gmail.com>
2024-07-20 18:35:12 +0300

    logo added in the splash screen

--- edugem/lib/screens/splash_screen.dart
+++ edugem/lib/screens/splash_screen.dart
@@ -6,52 +6,54 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue, // Set background color to blue
+      backgroundColor: Colors.blue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset(
+              'assets/images/logo.png', // Path to the logo image
+              width: 250,
+              height: 250,
+            ),
+            const SizedBox(height: 20),
             const Text(
               'Welcome to EduGem AI',
               style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white), // Set text color to white for contrast
+                  color: Colors.white),
             ),
             const SizedBox(height: 20),
             const Text(
               'Your personalized learning companion. Get started by logging in or registering an account.',
               textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white), // Set text color to white for contrast
+              style: TextStyle(fontSize: 16, color: Colors.white),
             ),
             const SizedBox(height: 20),
             SizedBox(
-              width: 200, // Set a fixed width for the buttons
+              width: 200,
               child: ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/login');
                 },
                 child: const Text(
                   'Login',
-                  style: TextStyle(
-                    color: Colors.blue,
-                  ),
+                  style: TextStyle(color: Colors.blue),
                 ),
               ),
             ),
             const SizedBox(height: 10),
             SizedBox(
-              width: 200, // Set a fixed width for the buttons
+              width: 200,
               child: ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/register');
                 },
-                child: const Text('Register',
-                    style: TextStyle(
-                      color: Colors.blue,
-                    )),
+                child: const Text(
+                  'Register',
+                  style: TextStyle(color: Colors.blue),
+                ),
               ),
             ),
           ],
